e8daa26ad028f41bfaebea42fd5c9b07b03d7ff5
abid hussain (abidhissain12678-design) <abidhissain12678@gmail.com>
2026-06-03 09:45:48 +0500

    all update

--- components/RestaurantWebsite.tsx
+++ components/RestaurantWebsite.tsx
@@ -44,7 +44,7 @@ const heroSlides = [
     title: "Family Combo",
     subtitle: "Share a generous family feast with favorites for everyone at the table.",
     cta: "Order Family Combo",
-    image: "https://images.unsplash.com/photo-1604908553991-0ad45d3819d7?q=80&w=1600&auto=format&fit=crop",
+    image: "https://scontent.flhe38-1.fna.fbcdn.net/v/t39.30808-6/462040870_919136030235768_8994001254767627136_n.png?stp=dst-png_s960x960&_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEb4XMyHZByRqXPNDqHwg4RX0SFus9UgvZfRIW6z1SC9mzdrGvESQayH0ONAhASBwH3QUO50CHHQx3BhTABvj0s&_nc_ohc=zkyw0XoOgNcQ7kNvwFHoJya&_nc_oc=AdrOEHnNgQfMPi4YaQN-M7gycGR5iljPeIdrjAW4M5Hc73XOJpG7OpUMs_hWfoRp0hU&_nc_zt=23&_nc_ht=scontent.flhe38-1.fna&_nc_gid=Nimkpfvj3BIlTXawFGBOCg&_nc_ss=7b2a8&oh=00_Af-QWxl-oZA0QJzyxTUVgdzJYcCwofQ0C7K0pV0bUS5k9g&oe=6A258502",
   },
   {
     title: "Free Delivery",
